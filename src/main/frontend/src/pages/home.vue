02eaf582c3d4e5a51418118572d9dcff15d6b447
Jonathan Buzzetti <jonathan.buzzetti@naranjax.com>
2021-02-24 00:25:51 -0300

    Armo bosquejo de front home.vue con publicaciones mock

--- src/main/frontend/src/pages/home.vue
+++ src/main/frontend/src/pages/home.vue
@@ -1,68 +1,81 @@
 <template>
   <f7-page name="home">
     <!-- Top Navbar -->
-    <f7-navbar large :sliding="false">
+    <f7-navbar>
       <f7-nav-left>
-        <f7-link icon-ios="f7:menu" icon-aurora="f7:menu" icon-md="material:menu" panel-open="left"></f7-link>
+        <f7-link
+          icon-ios="f7:menu"
+          icon-aurora="f7:menu"
+          icon-md="material:menu"
+          panel-open="left"
+        ></f7-link>
       </f7-nav-left>
-      <f7-nav-title sliding>Encontralo</f7-nav-title>
-      <f7-nav-right>
-        <f7-link icon-ios="f7:menu" icon-aurora="f7:menu" icon-md="material:menu" panel-open="right"></f7-link>
-      </f7-nav-right>
-      <f7-nav-title-large>Encontralo</f7-nav-title-large>
+      <f7-nav-title>Encontralo.com.ar</f7-nav-title>
     </f7-navbar>
     <!-- Toolbar-->
-    <f7-toolbar bottom>
-      <f7-link>Left Link</f7-link>
-      <f7-link>Right Link</f7-link>
-    </f7-toolbar>
+
     <!-- Page content-->
     <f7-block strong>
-      <p>Here is your blank Framework7 app. Let's see what we have here.</p>
-    </f7-block>
-    <f7-block-title>Navigation</f7-block-title>
-    <f7-list>
-      <f7-list-item link="/about/" title="About"></f7-list-item>
-      <f7-list-item link="/form/" title="Form"></f7-list-item>
-    </f7-list>
-
-    <f7-block-title>Modals</f7-block-title>
-    <f7-block strong>
-      <f7-row>
-        <f7-col width="50">
-          <f7-button fill raised popup-open="#my-popup">Popup</f7-button>
-        </f7-col>
-        <f7-col width="50">
-          <f7-button fill raised login-screen-open="#my-login-screen">Login Screen</f7-button>
-        </f7-col>
-      </f7-row>
+      <p>El buscador Nº1 de mascotas perdidas</p>
     </f7-block>
 
-    <f7-block-title>Panels</f7-block-title>
-    <f7-block strong>
-      <f7-row>
-        <f7-col width="50">
-          <f7-button fill raised panel-open="left">Left Panel</f7-button>
-        </f7-col>
-        <f7-col width="50">
-          <f7-button fill raised panel-open="right">Right Panel</f7-button>
-        </f7-col>
-      </f7-row>
-    </f7-block>
+    <f7-row class="demo-col-center-content">
+      <f7-col width="90" medium="40">
+        <f7-button large fill raised href="/publicar">Publicar</f7-button>
+      </f7-col>
+    </f7-row>
 
-    <f7-list>
-      <f7-list-item
-        title="Dynamic (Component) Route"
-        link="/dynamic-route/blog/45/post/125/?foo=bar#about"
-      ></f7-list-item>
-      <f7-list-item
-        title="Default Route (404)"
-        link="/load-something-that-doesnt-exist/"
-      ></f7-list-item>
-      <f7-list-item
-        title="Request Data & Load"
-        link="/request-and-load/user/123456/"
-      ></f7-list-item>
-    </f7-list>
+    <f7-block-title>Algunos perdidos</f7-block-title>
+    <f7-row>
+      <f7-col width="100" medium="33" xlarge="20">
+        <f7-card class="demo-card-header-pic">
+          <f7-card-header
+            class="no-border"
+            valign="bottom"
+            style="
+              background-image: url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJe2jzHNs4Ex_L2_Yi6HtpXICEp3PYQjnsNg&usqp=CAU);
+            "
+            >Rocko</f7-card-header
+          >
+          <f7-card-content>
+            <p class="date">Publicado el 21 de enero, 2021</p>
+            <p>Chihuaha, adulto mayor</p>
+            <p>Quilmes, Bs. As.</p>
+          </f7-card-content>
+        </f7-card>
+      </f7-col>
+      <f7-col width="100" medium="33" xlarge="20">
+        <f7-card class="demo-card-header-pic">
+          <f7-card-header
+            class="no-border"
+            valign="bottom"
+            style="
+              background-image: url(https://www.elcomercio.com/files/article_main/uploads/2018/05/16/5afc9aa25b648.jpeg);
+            "
+            >Laila</f7-card-header
+          >
+          <f7-card-content>
+            <p class="date">Publicado el 23 de enero, 2021</p>
+            <p>Sharpei, cachorro</p>
+            <p>Vicente López, Bs. As.</p>
+          </f7-card-content>
+        </f7-card>
+      </f7-col>
+    </f7-row>
   </f7-page>
 </template>
+
+<style>
+.demo-col-center-content {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.demo-card-header-pic .card-header {
+  height: 140px;
+  background-size: cover;
+  background-position: center;
+  color: #fff;
+  text-shadow: 0 0 3px #000;
+}
+</style>
