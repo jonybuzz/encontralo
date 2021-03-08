f1a3f2e45145f8d82919470896a59d7b40b48519
Jonathan Buzzetti <jonathan.buzzetti@naranjax.com>
2021-03-08 03:20:25 -0300

    Mejoras slide

--- src/main/frontend/src/pages/home.vue
+++ src/main/frontend/src/pages/home.vue
@@ -9,7 +9,7 @@
 
     <!-- Page content-->
     <f7-swiper>
-      <f7-swiper-slide>El buscador Nº1 de mascotas perdidas</f7-swiper-slide>
+      <f7-swiper-slide class="enc-slide">El buscador Nº1 de mascotas perdidas</f7-swiper-slide>
     </f7-swiper>
 
     <f7-block>
@@ -26,22 +26,23 @@
     <f7-block>
       <f7-row class="enc-col-center-content">
         <f7-col width="90" xsmall="90" small="60" medium="33" xlarge="20" class="enc-main-button">
-          <f7-button large raised href="/anunciar-perdido" icon-f7="search">Buscar anuncio</f7-button>
+          <f7-button large raised href="/anunciar-perdido" icon-f7="search">Buscar</f7-button>
         </f7-col>
       </f7-row>
     </f7-block>
 
-    <f7-block-title>Recientes</f7-block-title>
+    <f7-block-title>Anuncios recientes</f7-block-title>
     <f7-row>
       <f7-col width="100" medium="33" xlarge="20">
         <f7-card class="demo-card-header-pic">
           <f7-card-header
-            class="no-border"
-            valign="bottom"
-            style="
+              class="no-border"
+              valign="bottom"
+              style="
               background-image: url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJe2jzHNs4Ex_L2_Yi6HtpXICEp3PYQjnsNg&usqp=CAU);
             "
-            >Rocko</f7-card-header
+          >Rocko
+          </f7-card-header
           >
           <f7-card-content>
             <p class="date">Publicado el 21 de enero, 2021</p>
@@ -53,12 +54,13 @@
       <f7-col width="100" medium="33" xlarge="20">
         <f7-card class="demo-card-header-pic">
           <f7-card-header
-            class="no-border"
-            valign="bottom"
-            style="
+              class="no-border"
+              valign="bottom"
+              style="
               background-image: url(https://www.elcomercio.com/files/article_main/uploads/2018/05/16/5afc9aa25b648.jpeg);
             "
-            >Laila</f7-card-header
+          >Laila
+          </f7-card-header
           >
           <f7-card-content>
             <p class="date">Publicado el 23 de enero, 2021</p>
@@ -70,12 +72,13 @@
       <f7-col width="100" medium="33" xlarge="20">
         <f7-card class="demo-card-header-pic">
           <f7-card-header
-            class="no-border"
-            valign="bottom"
-            style="
+              class="no-border"
+              valign="bottom"
+              style="
               background-image: url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJe2jzHNs4Ex_L2_Yi6HtpXICEp3PYQjnsNg&usqp=CAU);
             "
-            >Rocko</f7-card-header
+          >Rocko
+          </f7-card-header
           >
           <f7-card-content>
             <p class="date">Publicado el 21 de enero, 2021</p>
@@ -87,12 +90,13 @@
       <f7-col width="100" medium="33" xlarge="20">
         <f7-card class="demo-card-header-pic">
           <f7-card-header
-            class="no-border"
-            valign="bottom"
-            style="
+              class="no-border"
+              valign="bottom"
+              style="
               background-image: url(https://www.elcomercio.com/files/article_main/uploads/2018/05/16/5afc9aa25b648.jpeg);
             "
-            >Laila</f7-card-header
+          >Laila
+          </f7-card-header
           >
           <f7-card-content>
             <p class="date">Publicado el 23 de enero, 2021</p>
@@ -113,11 +117,12 @@
   color: #fff;
   text-shadow: 0 0 3px #000;
 }
+
 .enc-main-button {
   margin: 12px;
 }
-.swiper-slide {
-  background: #fff;
+
+.enc-slide {
   text-align: center;
   font-size: 2em;
   line-height: 350px;
@@ -126,6 +131,15 @@
   color: white;
   text-shadow: 0 0 3px #000;
   background-image: url("/src/static/img/slide_1.jpg");
+  background-position: center;
+}
+
+@media only screen and (max-width: 568px) {
+  .enc-slide {
+    line-height: 280px;
+    background-image: url("/src/static/img/slide_1_mobile.jpg");
+    background-size: cover;
+  }
 }
 
 </style>
