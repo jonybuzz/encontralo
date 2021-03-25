f737081534a46647497136d6ba6d4d966a8adba0
Jonathan Buzzetti <jonathan.buzzetti@naranjax.com>
2021-03-25 20:13:38 -0300

    agrego sqls de datos para prod (wikipedia)

---
+++ src/main/data/colores.sql
@@ -0,0 +1,9 @@
+INSERT INTO color
+(nombre   , codigo_hex) VALUES
+('Beige'  , 'F9E1C9'  ),
+('Negro'  , '0C0C0C'  ),
+('Blanco' , 'FBFBFB'  ),
+('Gris'   , 'BFBFBF'  ),
+('Marrón' , '764323'  ),
+('Naranja', 'FFAD33'  );
+
