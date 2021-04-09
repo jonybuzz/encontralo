4d0409e07e30050cf2f72763e09ae6550853f5e9
Adrian Zubieta H <adrianzh@onready.com.ar>
2021-04-08 21:29:28 -0300

    elimine imports sin usar

--- src/main/java/com/jonybuzz/encontralo/dto/NuevoAnuncioDto.java
+++ src/main/java/com/jonybuzz/encontralo/dto/NuevoAnuncioDto.java
@@ -7,7 +7,6 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import java.util.HashSet;
 import java.util.Set;
