c65e0f1dd1c55673ba7487f5c21f554916bbda4f
Adrian Zubieta H <adrianzh@onready.com.ar>
2021-04-08 21:38:54 -0300

    elimine la excepcion sin uso

--- src/main/java/com/jonybuzz/encontralo/service/exception/AnuncioIncompletoException.java
+++
@@ -1,9 +0,0 @@
-package com.jonybuzz.encontralo.service.exception;
-
-public class AnuncioIncompletoException extends Exception {
-
-    public AnuncioIncompletoException(String msg) {
-        super(msg);
-    }
-
-}
